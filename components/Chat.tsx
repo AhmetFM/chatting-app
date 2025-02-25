10dccf0607b6c9824dd8d941e179b0682a0d32f2
Ahmet Furkan Meric <afurkanmeric@gmail.com>
2025-02-25 15:58:21 +0300

    Updated Chat Ui

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Info, Phone, Video } from "lucide-react";
+import {
+  Camera,
+  Image,
+  Info,
+  Mic,
+  Phone,
+  SmilePlus,
+  Video,
+} from "lucide-react";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e: EmojiClickData) => {
+    setText((prev) => prev + e.emoji);
+  };
+
   return (
-    <div className="flex-[2] border-x border-gray-700 h-full ">
+    <div className="flex-[2] border-x border-gray-700 h-full flex flex-col">
       <div className="p-5 flex items-center justify-between border-b border-gray-700">
         <div className="flex items-center gap-5">
           <Avatar className="w-[60px] h-[60px] object-cover">
@@ -24,8 +41,70 @@ const Chat = () => {
           <Info size={20} />
         </div>
       </div>
-      <div></div>
-      <div></div>
+      {/* Chat Part */}
+      <div className="flex-1 p-5 overflow-scroll flex flex-col gap-5">
+        <div className="message">
+          <Avatar className="w-[30px] h-[30px] object-cover">
+            <AvatarImage src="https://github.com/ahmetfm.png" />
+            <AvatarFallback>AM</AvatarFallback>
+          </Avatar>
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              consequuntur totam voluptatum accusantium unde itaque optio
+              ducimus, perferendis, praesentium, corporis ipsum odio deleniti!
+              Culpa assumenda, numquam dignissimos repellat aspernatur
+              consequuntur.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          {/* <Avatar className="w-[60px] h-[60px] object-cover">
+            <AvatarImage src="https://github.com/ahmetfm.png" />
+            <AvatarFallback>AM</AvatarFallback>
+          </Avatar> */}
+          <div className="texts">
+            <img src="/images/bg-3.jpg" alt="" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              consequuntur totam voluptatum accusantium unde itaque optio
+              ducimus, perferendis, praesentium, corporis ipsum odio deleniti!
+              Culpa assumenda, numquam dignissimos repellat aspernatur
+              consequuntur.
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      {/* Bottom Part */}
+      <div className="p-5 flex items-center justify-between border-t border-[#dddddd35] gap-5 mt-auto">
+        <div className="flex gap-5">
+          <Image className="cursor-pointer" size={20} />
+          <Camera className="cursor-pointer" size={20} />
+          <Mic className="cursor-pointer" size={20} />
+        </div>
+        <input
+          className="flex-1 bg-zinc-900/80 border-none outline-none text-white p-5 rounded-[10px] text-base"
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="relative">
+          <SmilePlus
+            className="cursor-pointer"
+            size={20}
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="absolute bottom-[50px] left-0">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="bg-[#5183fe] text-white px-[10px] py-[20px] border-none rounded-[5px] cursor-pointer">
+          Send
+        </button>
+      </div>
     </div>
   );
 };
